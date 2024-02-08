2eff0219d39c8fce4500d1c32c22813ba97458f8
Tornayyydo <sandrosandala22@gmail.com>
2024-02-08 19:17:39 +0400

    sidebar implemented

--- angular-project/src/app/shell/sidebar/nav-data.ts
+++ angular-project/src/app/shell/sidebar/nav-data.ts
@@ -1,7 +1,37 @@
 export const navbarData = [
     {
-        routeLink: '/dashboard',
+        routeLink: '',
         icon: 'home',
-        label: 'Dashboard'
-    }
+        label: 'Home'
+    },
+    {
+        routeLink: '',
+        icon: 'grid_view',
+        label: 'Categories'
+    },
+    {
+        routeLink: '',
+        icon: 'home',
+        label: 'Home'
+    },
+    {
+        routeLink: '',
+        icon: 'home',
+        label: 'Home'
+    },
+    {
+        routeLink: '',
+        icon: 'home',
+        label: 'Home'
+    },
+    {
+        routeLink: '',
+        icon: 'login',
+        label: 'Sign In'
+    },
+    {
+        routeLink: '',
+        icon: 'logout',
+        label: 'Log Out'
+    },
 ]
--- angular-project/src/app/shell/sidebar/sidebar.component.ts
+++ angular-project/src/app/shell/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
+import { SidebarService } from 'src/app/services/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,6 +8,19 @@ import { navbarData } from './nav-data';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  collapsed = false;
+  constructor(public sidebarService: SidebarService, private elementRef: ElementRef) {}
+
   navData = navbarData;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    console.log(this.sidebarService.collapsed);
+    const target = event.target as HTMLElement;
+    const sidebarElement = this.elementRef.nativeElement.querySelector('.sidenav');
+    const isClickInsideSidebar = sidebarElement && sidebarElement.contains(target);
+    const isBelowHeightLimit = event.clientY > 5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    if (!isClickInsideSidebar && !this.sidebarService.collapsed && isBelowHeightLimit) {
+      this.sidebarService.collapsed = true
+    }
+  }
 }
