b99f4200d3f69b8640f5bc2212b4454b41231c12
nodo6777 <martolekinodari1998@gmail.com>
2024-02-09 14:09:04 +0400

    login

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -3,17 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shell/home/home.component';
 import { FoodPageComponent } from './shell/food-page/food-page.component';
 import { CartPageComponent } from './shell/cart-page/cart-page.component';
+import { LoginComponent } from './shell/login/login.component';
+import { RegisterComponent } from './shell/register/register.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'search/:searchTerm', component: HomeComponent},
-  {path:'tag/:tag', component: HomeComponent},
-  {path:'food/:id', component:FoodPageComponent},
-  {path:'cart-page', component: CartPageComponent}
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  { path: 'food/:id', component: FoodPageComponent },
+  { path: 'cart-page', component: CartPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
