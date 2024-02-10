d211ffd307150c02677a67aa09d6ec9708ae84e1
nodo6777 <martolekinodari1998@gmail.com>
2024-02-10 22:22:37 +0400

    in sidebar log out

--- angular-project/src/app/shell/sidebar/sidebar.component.ts
+++ angular-project/src/app/shell/sidebar/sidebar.component.ts
@@ -1,29 +1,50 @@
 import { Component, ElementRef, HostListener } from '@angular/core';
 import { navbarData } from './nav-data';
 import { SidebarService } from 'src/app/services/sidebar/sidebar.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-  constructor(public sidebarService: SidebarService, private elementRef: ElementRef) {}
+  constructor(
+    public sidebarService: SidebarService,
+    private elementRef: ElementRef,
+    private authService: AuthService
+  ) {}
 
   navData = navbarData;
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logoutUser();
+  }
 
   @HostListener('document:click', ['$event'])
   onClick(event: MouseEvent) {
     const target = event.target as HTMLElement;
-    const sidebarElement = this.elementRef.nativeElement.querySelector('.sidenav');
-    const isClickInsideSidebar = sidebarElement && sidebarElement.contains(target);
-    const isBelowHeightLimit = event.clientY > 5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
-    if (!isClickInsideSidebar && !this.sidebarService.collapsed && isBelowHeightLimit) {
-      this.sidebarService.collapsed = true
+    const sidebarElement =
+      this.elementRef.nativeElement.querySelector('.sidenav');
+    const isClickInsideSidebar =
+      sidebarElement && sidebarElement.contains(target);
+    const isBelowHeightLimit =
+      event.clientY >
+      5 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    if (
+      !isClickInsideSidebar &&
+      !this.sidebarService.collapsed &&
+      isBelowHeightLimit
+    ) {
+      this.sidebarService.collapsed = true;
     }
   }
 
-  onClicked(){
-    this.sidebarService.collapsed = true
+  onClicked() {
+    this.sidebarService.collapsed = true;
   }
 }
